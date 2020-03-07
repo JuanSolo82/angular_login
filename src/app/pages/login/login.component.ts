7d6e0150f727fb2dfe4599d40372104afda773dc
jmarinan <jmarinan@tabsa.cl>
2020-03-07 12:05:54 -0300

    Recordar datos login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,11 +12,17 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 	usuario: UsuarioModel = new UsuarioModel();
+	recordarme = false;
 	
 	constructor(private auth: AuthService,
 				private router: Router) { }
 
-	ngOnInit() {}
+	ngOnInit() {
+		if (localStorage.getItem('email')){
+			this.usuario.email = localStorage.getItem('email');
+			this.recordarme = true;
+		}
+	}
 
 	login(form: NgForm){
 		if (form.invalid){
@@ -35,6 +41,9 @@ export class LoginComponent implements OnInit {
 			.subscribe(resp => {
 				console.log(resp);
 				Swal.close();
+				if (this.recordarme){
+					localStorage.setItem('email', this.usuario.email);
+				}
 				this.router.navigateByUrl('/home')
 			}, (err) => {
 				Swal.fire({
